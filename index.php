bb89e8fa6d8c58478d823d5ee9d3ef1afd34677a
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-13 10:22:02 +0100

    boucle 2

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP, les boucles</title>
+  </head>
+  <body>
+    <?php
+    $count= 0;
+    $test= 12;
+
+    while ($count <= 20) {
+      # code...
+      echo $count * $test;
+      $count++;
+    }
+
+     ?>
+  </body>
+</html>
